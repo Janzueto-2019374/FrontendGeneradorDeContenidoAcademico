67641e94b6446199bd10707d74b8399d1b4a4772
AlvaroAnz <128559200+AlvaroAnz@users.noreply.github.com>
2023-10-23 11:16:37 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
+  const [searchText, setSearchText] = useState('');
+  const [selectedOption, setSelectedOption] = useState('');
+  const [comment, setComment] = useState('');
+
+  const handleGenerate = () => {
+    // Agregar la lógica para el botón Generar aquí
+  };
+
+  const handleView = () => {
+    // Agregar la lógica para el botón Vista aquí
+  };
+
+  const handleGenerateFooter = () => {
+    // Agregar la lógica para el botón Generar del pie de página aquí
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <input
+          type="text"
+          placeholder="Buscar..."
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <select value={selectedOption} onChange={(e) => setSelectedOption(e.target.value)}>
+          <option value="">Selecciona...</option>
+          <option value="opcion1">Opción 1</option>
+          <option value="opcion2">Opción 2</option>
+        </select>
+        <Button variant="primary" onClick={handleGenerate}>Generar</Button>
+        <Button variant="primary" onClick={handleView}>Vista</Button>
       </header>
+      <main>
+        {/* Contenido del mediocentro */}
+      </main>
+      <footer>
+        <input
+          type="text"
+          placeholder="Agregar un comentario..."
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        />
+        <button onClick={handleGenerateFooter}>Generar</button>
+      </footer>
     </div>
   );
 }
